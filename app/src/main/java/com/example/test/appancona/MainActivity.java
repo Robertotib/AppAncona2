8cc9d56ab0077e1cfb36cf6dc06830b2b937b67c
Robertotib <tiburzi.roberto93@gmail.com>
2018-11-30 19:14:53 +0100

    prova

--- app/src/main/java/com/example/test/appancona/MainActivity.java
+++ app/src/main/java/com/example/test/appancona/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.test.appancona;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.actionbar,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        switch(id)
+        {
+            case R.id.MENU_1:
+
+                break;
+
+            case R.id.MENU_2:
+        }
+        return false;
     }
 }
