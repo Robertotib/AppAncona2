872026cbae0927b80544634285e0dbb05a883b16
Robertotib <tiburzi.roberto93@gmail.com>
2018-12-04 19:44:20 +0100

    activity

--- app/src/main/java/com/example/test/appancona/Punti_interesse1Activity.java
+++ app/src/main/java/com/example/test/appancona/Punti_interesse1Activity.java
@@ -1,11 +1,11 @@
 package com.example.test.appancona;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class Punti_interesse1Activity extends AppCompatActivity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.punti_interesse1);
