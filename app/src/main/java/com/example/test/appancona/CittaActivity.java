ce754664c10fdc1e02604967cd6d67b92c42b314
Robertotib <tiburzi.roberto93@gmail.com>
2018-12-14 12:06:07 +0100

    Come visitarla activity Tradizioni Activity

--- app/src/main/java/com/example/test/appancona/CittaActivity.java
+++ app/src/main/java/com/example/test/appancona/CittaActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class CittaActivity extends AppCompatActivity {
@@ -14,10 +15,14 @@ public class CittaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.citta);
         addListenerOnNegozi();
+        addListenerOnVisitarla();
+        addListenerOnTradizioni();
     }
     public void addListenerOnNegozi(){
         ImageView a;
+        TextView b;
         a  =  findViewById(R.id.image3);
+        b = findViewById(R.id.label3);
         a.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,6 +33,65 @@ public class CittaActivity extends AppCompatActivity {
             }
         });
 
+        b.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i;
+                i = new Intent(CittaActivity.this,Negozi_tipiciActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+    public void addListenerOnVisitarla(){
+        ImageView a;
+        TextView b;
+        a  =  findViewById(R.id.image2);
+        b = findViewById(R.id.label2);
+        a.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i;
+                i = new Intent(CittaActivity.this,VisitarlaActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        b.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i;
+                i = new Intent(CittaActivity.this,VisitarlaActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+    public void addListenerOnTradizioni(){
+        ImageView a;
+        TextView b;
+        a  =  findViewById(R.id.image4);
+        b = findViewById(R.id.label4);
+        a.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i;
+                i = new Intent(CittaActivity.this,TradizioniActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        b.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i;
+                i = new Intent(CittaActivity.this,TradizioniActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/test/appancona/TradizioniActivity.java
@@ -0,0 +1,11 @@
+package com.example.test.appancona;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TradizioniActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tradizioni);
+    }
+}
