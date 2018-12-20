aa4d9b2fa2f0c91cc6b66b1bc0e4d1041daec45a
Robertotib <tiburzi.roberto93@gmail.com>
2018-12-20 17:57:18 +0100

    Tentativo di inserimento mappe

--- app/src/main/java/com/example/test/appancona/Punti_interesse/Punti_interesse4Activity.java
+++ app/src/main/java/com/example/test/appancona/Punti_interesse/Punti_interesse4Activity.java
@@ -1,15 +1,39 @@
 package com.example.test.appancona.Punti_interesse;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import com.example.test.appancona.*;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Punti_interesse4Activity extends AppCompatActivity {
+public class Punti_interesse4Activity extends FragmentActivity implements OnMapReadyCallback  {
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.punti_interesse4);
         String t =getIntent().getStringExtra("nome");
         setTitle(t);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng firenze = new LatLng(43.776366, 11.247822);
+        mMap.addMarker(new MarkerOptions().position(firenze).title("Siamo a Firenze!"));
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(firenze).zoom(15).build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
+
