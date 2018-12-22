90f3f2c30bb2e067f01ac36246ffd115c90c4a14
Robertotib <tiburzi.roberto93@gmail.com>
2018-12-22 13:07:54 +0100

    Activity ricerca_punti_interesse filtraggio (incompleto)

--- app/src/main/java/com/example/test/appancona/Ricerca/RicercaActivity.java
+++ app/src/main/java/com/example/test/appancona/Ricerca/RicercaActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.test.appancona.MainActivity;
@@ -20,8 +21,19 @@ public class RicercaActivity extends AppCompatActivity {
     }
     public void addListenerOnPuntiInteresse(){
         TextView a;
+        ImageView b;
         a  =  findViewById(R.id.label1);
+        b  = findViewById(R.id.image1);
         a.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i;
+                i = new Intent(RicercaActivity.this,Ricerca_punti_interesseActivity.class);
+                startActivity(i);
+
+            }
+        });
+        b.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i;
--- app/src/main/java/com/example/test/appancona/Ricerca/Ricerca_punti_interesseActivity.java
+++ app/src/main/java/com/example/test/appancona/Ricerca/Ricerca_punti_interesseActivity.java
@@ -1,20 +1,28 @@
 package com.example.test.appancona.Ricerca;
 
+import android.database.Cursor;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.test.appancona.Database.DBManager;
 import com.example.test.appancona.MappaActivity;
 import com.example.test.appancona.MappaActivity.*;
 import com.example.test.appancona.R;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -22,8 +30,29 @@ public class Ricerca_punti_interesseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ricerca_punti_interesse);
-        TextView tv = findViewById(R.id.distanza);
-        tv.setText(CalcoloDistanza("via piave 5 ancona ","via piave 4 ancona").toString());
+        valoreSeekBar(R.id.distanza,R.id.valoredist);
+        SeekBar dis = findViewById(R.id.distanza);
+        dis.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                valoreSeekBar(R.id.distanza,R.id.valoredist);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        inizializzaSpinner();
+        addListenerRicerca();
+
+      /*  TextView tv = findViewById(R.id.distanza);
+        tv.setText(CalcoloDistanza("via piave 5 ancona ","via piave 4 ancona").toString());*/
 
     }
     public LatLng getSingleLocationFromAddress(String strAddress)
@@ -67,5 +96,43 @@ public class Ricerca_punti_interesseActivity extends AppCompatActivity {
         locend.setLongitude(end.longitude);
     Float distanza = locend.distanceTo(locstart);
     return distanza;
+    }
+
+    public void valoreSeekBar(int idseeker, int idtext )
+    {
+    SeekBar dist = findViewById(idseeker);
+    Integer numero =dist.getProgress()+100;
+    TextView valore = findViewById(idtext);
+        valore.setText(numero.toString()+ " m");
+    }
+    public void inizializzaSpinner()
+    {
+        List<String> spinnerArray =  new ArrayList<String>();
+        DBManager db = new DBManager(this);
+        Cursor tipi = db.tipiPuntiinteresse();
+        spinnerArray.add("Tutte");
+        while (tipi.moveToNext())
+        {
+            spinnerArray.add(tipi.getString(tipi.getColumnIndex("_id")));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.categoria);
+        sItems.setAdapter(adapter);
+    }
+    public void addListenerRicerca()
+    {
+        Button cerca = findViewById(R.id.ricerca);
+        cerca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Spinner ris =findViewById(R.id.categoria);
+                TextView di =findViewById(R.id.valoredist);
+                Toast.makeText(Ricerca_punti_interesseActivity.this, di.getText()+" "+ ris.getSelectedItem().toString() , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
