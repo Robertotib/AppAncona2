04eb8c4ea3eee11c02411fb67eb4bb3d21d131c6
Robertotib <tiburzi.roberto93@gmail.com>
2019-01-03 13:38:41 +0100

    Mappa che si aggiorna durante il movimento

--- app/src/main/java/com/example/test/appancona/MappaActivity.java
+++ app/src/main/java/com/example/test/appancona/MappaActivity.java
@@ -1,11 +1,20 @@
 package com.example.test.appancona;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -31,40 +40,114 @@ import org.osmdroid.views.overlay.Polyline;
 
 import java.io.IOException;
 import java.math.RoundingMode;
+import java.security.Provider;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class MappaActivity extends AppCompatActivity  {
-
+public class MappaActivity extends AppCompatActivity {
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location myLocation;
     private Road road = null;
+    private String indirizzo;
+    private MapView map;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mappa);
-        String t =getIntent().getStringExtra("nome");
-        String ind =getIntent().getStringExtra("indirizzo");
+        String t = getIntent().getStringExtra("nome");
+        indirizzo = getIntent().getStringExtra("indirizzo");
+        map =  findViewById(R.id.map);
         setTitle(t);
-        inizializzaMappa(ind);
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("My Location: ", location.toString());
+                myLocation = location;
+                percorso(myLocation);
+                Indicazioni(road);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        if(Build.VERSION.SDK_INT < 23)
+        {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }else {
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+        myLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        inizializzaMappa();
+        percorso(myLocation);
         Indicazioni(road);
 
     }
 
-    public void inizializzaMappa(String indirizzo){
-        GPSTracker gps = new GPSTracker(this);
-        LatLng luogo = getSingleLocationFromAddress(indirizzo+" ancona",this);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+        {
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
+
+    public void inizializzaMappa(){
+
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        /**
-         * TUTORIAL 0
-         */
-        MapView map = (MapView) findViewById(R.id.map);
+
         map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        GeoPoint startPoint = new GeoPoint(gps.getLatitude(),gps.getLongitude());
+
+
+
+
+    }
+    public void percorso (Location location)
+    {
+        GeoPoint startPoint = new GeoPoint(location);
+        LatLng luogo = getSingleLocationFromAddress(indirizzo+" ancona",this);
         IMapController mapController = map.getController();
         mapController.setZoom(9.0);
         mapController.setCenter(startPoint);
@@ -74,29 +157,22 @@ public class MappaActivity extends AppCompatActivity  {
         startMarker.setTitle("Start point");
         map.getOverlays().add(startMarker);
         map.invalidate();
-        /**
-         * TUTORIAL 1
-         */
-        //RoadManager roadManager = new MapQuestRoadManager("3Vky6DRJBdJ0JdN4twC0Amiw3JV0yFMF");
-        //roadManager.addRequestOption("routeType=fastest");
-        RoadManager roadManager = new GraphHopperRoadManager("0b4fbc1c-22f4-4a8e-af38-3a4d422a395c&locale=it",true);
-        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+    RoadManager roadManager = new GraphHopperRoadManager("0b4fbc1c-22f4-4a8e-af38-3a4d422a395c&locale=it",true);
+    ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
         waypoints.add(startPoint);
-        GeoPoint endPoint = new GeoPoint(luogo.latitude, luogo.longitude);
+    GeoPoint endPoint = new GeoPoint(luogo.latitude, luogo.longitude);
         waypoints.add(endPoint);
-        Marker endMarker = new Marker(map);
+    Marker endMarker = new Marker(map);
         endMarker.setPosition(endPoint);
         endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         endMarker.setTitle("End point");
         map.getOverlays().add(endMarker);
-        Road road = roadManager.getRoad(waypoints);
-        Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+    Road road = roadManager.getRoad(waypoints);
+    Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
         map.getOverlays().add(roadOverlay);
         map.invalidate();
         this.road=road;
-
-    }
-
+}
     public LatLng getSingleLocationFromAddress(String strAddress, Context c)
     {
         Geocoder coder = new Geocoder(c, Locale.getDefault());
